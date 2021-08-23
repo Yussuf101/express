d0945ba4fca731b93f66187c707ff7954f50fb27
Yussuf101 <32193666+Yussuf101@users.noreply.github.com>
2021-08-23 12:46:23 +0100

    changed gitingore and added urls, updated index.js file

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello world");
+});
+
+app.get("/about", (req, res) => {
+    res.status(200).send("This is the about route");
+});
+
+app.get("/users/:username", (req, res) => {
+    console.log(req.query);
+    res.status(200).send(`You requested information about ${req.params.username}: ${req.query.age}`);
+});
+
+app.get("/:username/:project", (req, res) => {
+    res.status(200).send(`You requested information about ${req.params.project} created by ${req.params.username}`);
+});
+
+app.listen(port, () => {
+    console.log("App is online");
+});
