cc19140e9c42f53d504f1002f2a4c46bd3106c82
Yussuf101 <32193666+Yussuf101@users.noreply.github.com>
2021-08-24 11:17:32 +0100

    created the password and checker

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const saltRounds = 10;
+
 app.use(express.json()); // This ensures input is considered to be json
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello world");
+});
+
+app.get("/register", (req,res)=>{
+    bcrypt.genSalt(saltRounds,(err, salt)=>{
+        bcrypt.hash(req.body.password, salt, (err, hash)=>{
+            if(err){
+                res.status(500).json({"message": `Somthing went wrong`, "error": err});
+            }
+            res.status(201).json({"message":`Hashed password is: ${hash}`})
+        });
+    });
+});
+
+app.post("/register", (req, res)=>{
+    bcrypt.genSalt(saltRounds, (err, salt)=>{
+        if(err){
+            res.status(500).json({"message": `Somthing went wrong`, "error": err});
+        }
+        bcrypt.hash(req,body.password, salt, (err,hash)=>{
+            if(result){
+                res.status(201).json({"message":`Hashed password is: ${hash}`});
+            }else{
+                res.status(201).json({"message":`Hashed password is: ${hash}`});
+            }
+        });
+    });
 });
 
 app.post("/:username/", (req, res) => {
